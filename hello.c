4ff4498c876f618a5580ed676eb2dc22fc4c1e84
qreimer <54610032+qreimer@users.noreply.github.com>
2019-08-29 08:25:56 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Quinn Reimer
+ * Date: 2018/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Quinn Reimer\n");
+
+  return 0;
+}
